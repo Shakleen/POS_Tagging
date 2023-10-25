de9a1d63d7e45a79eaa1b7ce063e6546f025692e
Shakleen Ishfar <shakleenishfar@gmail.com>
2023-10-25 09:19:46 -0400

    Integrate train and eval to model
    The train and eval step is now a part of the model itself.

--- src/module/module.py
+++ src/module/module.py
@@ -1,7 +1,6 @@
 import torch
 from torch import nn
 
-from ..utils.progress_board import ProgressBoard
 from ..utils.hyperparameters import HyperParameters
 
 
@@ -11,11 +10,10 @@ class Module(nn.Module, HyperParameters):  # @save
     def __init__(self, plot_train_per_epoch=2, plot_valid_per_epoch=1):
         super().__init__()
         self.save_hyperparameters()
-        self.board = ProgressBoard()
 
     def loss(self, y_hat, y):
         raise NotImplementedError
-    
+
     def accuracy(self, y_hat, y):
         raise NotImplementedError
 
@@ -23,39 +21,48 @@ class Module(nn.Module, HyperParameters):  # @save
         assert hasattr(self, 'net'), 'Neural network is not defined'
         return self.net(X)
 
-    def plot(self, key, value, train: bool):
-        """Plot a point in animation"""
-        assert hasattr(self, 'trainer'), 'Trainer is not defined'
-        self.board.xlabel = 'epoch'
-
-        if train:
-            x = self.trainer.train_batch_idx / \
-                self.trainer.num_train_batches
-            n = self.trainer.num_train_batches / \
-                self.plot_train_per_epoch
-        else:
-            x = self.trainer.epoch + 1
-            n = self.trainer.num_val_batches / \
-                self.plot_valid_per_epoch
-
-        self.board.draw(x, value.detach().numpy(),
-                        ('train_' if train else 'val_') + key,
-                        every_n=int(n))
-
     def training_step(self, batch):
-        l = self.loss(self(*batch[:-1]), batch[-1])
-        self.plot('loss', l, train=True)
-        return l
+        # Get features and labels
+        X, y = batch
+
+        # Reset gradients to zero
+        self.optim.zero_grad()
+
+        # Make predictions
+        y_hat = self(X)
+
+        # Reshape predictions and labels to fit loss function
+        y_hat = y_hat.reshape((-1, y_hat.shape[-1]))
+        y = y.reshape(-1)
+
+        # Calculate loss and accuracy of predictions
+        loss = self.loss(y_hat, y)
+        acc = self.accuracy(y_hat, y)
+
+        # Back propagation
+        loss.backward()
+
+        # Update parameters
+        self.optim.step()
+
+        return loss, acc
 
     def validation_step(self, batch):
-        l = self.loss(self(*batch[:-1]), batch[-1])
-        self.plot('loss', l, train=False)
+        # Get features and labels
+        X, y = batch
+
+        # Make predictions
+        y_hat = self(X)
+
+        # Reshape predictions and labels to fit loss function
+        y_hat = y_hat.reshape((-1, y_hat.shape[-1]))
+        y = y.reshape(-1)
+
+        # Calculate loss and accuracy of predictions
+        loss = self.loss(y_hat, y)
+        acc = self.accuracy(y_hat, y)
+
+        return loss, acc
 
     def configure_optimizers(self):
         raise NotImplementedError
-
-    def apply_init(self, inputs, init=None):
-        self.forward(*inputs)
-
-        if init is not None:
-            self.net.apply(init)
--- src/utils/loops.py
+++ src/utils/loops.py
@@ -25,28 +25,7 @@ def train_loop(model: Module, iterator: Generator):
 
     # Iterate over batches
     for batch in iterator:
-        # Get features and labels
-        X, y = batch
-
-        # Reset gradients to zero
-        model.optim.zero_grad()
-
-        # Make predictions
-        y_hat = model(X)
-
-        # Reshape predictions and labels to fit loss function
-        y_hat = y_hat.reshape((-1, y_hat.shape[-1]))
-        y = y.reshape(-1)
-
-        # Calculate loss and accuracy of predictions
-        loss = model.loss(y_hat, y)
-        acc = model.accuracy(y_hat, y)
-
-        # Back propagation
-        loss.backward()
-
-        # Update parameters
-        model.optim.step()
+        loss, acc = model.training_step(batch)
 
         # Add to total loss
         epoch_loss += loss.item()
@@ -66,19 +45,7 @@ def evaluation_loop(model: Module, iterator: Generator):
     with torch.no_grad():
         # Iterate over batches
         for batch in iterator:
-            # Get features and labels
-            X, y = batch
-
-            # Make predictions
-            y_hat = model(X)
-
-            # Reshape predictions and labels to fit loss function
-            y_hat = y_hat.reshape((-1, y_hat.shape[-1]))
-            y = y.reshape(-1)
-
-            # Calculate loss and accuracy of predictions
-            loss = model.loss(y_hat, y)
-            acc = model.accuracy(y_hat, y)
+            loss, acc = model.validation_step(batch)
 
             # Add to total loss
             epoch_loss += loss.item()
@@ -99,7 +66,7 @@ def train_epochs(model: Module, data: DataModule, epochs: int):
     Returns:
         Tuple: List of loss and accuracy values.
     """
-    losses, accuracies = [], []
+    losses, accuracies = [None] * epochs, [None] * epochs
 
     for epoch in range(epochs):
         start_time = time.time()
@@ -109,8 +76,11 @@ def train_epochs(model: Module, data: DataModule, epochs: int):
 
         elapsed = time.time() - start_time
         
+        losses[epoch] = (train_loss, val_loss)
+        accuracies[epoch] = (train_acc, val_acc)
+
         print(f"Epoch: {epoch+1:02} in ({elapsed:4.2f}) secs")
         print(f'\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc*100:.2f}%')
         print(f'\t Val. Loss: {val_loss:.3f} |  Val. Acc: {val_acc*100:.2f}%')
-        
+
     return losses, accuracies
